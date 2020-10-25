86ee883f145073a37740cef12ed7f47157e285b7
Madu97 <2018is050@stu.ucsc.cmb.ac.lk>
2020-10-25 15:23:37 +0530

    commit

--- src/java/Controller/Admin/UserMgt.java
+++ src/java/Controller/Admin/UserMgt.java
@@ -5,8 +5,13 @@
  */
 package Controller.Admin;
 
+import DB.DB;
+import DbClasses.Client;
+import DbClasses.Photographer;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,13 +23,53 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class UserMgt extends HttpServlet {
 
-
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
-            String search = request.getParameter("searchBar");
-    }
 
-  
+        String email = request.getParameter("searchBar");
+
+        try {
+
+            ResultSet client = DB.search("SELECT * FROM Client Where Email = '" + email + "' ");
+            ResultSet photographer = DB.search("SELECT * FROM Photographer Where Email = '" + email + "' ");
+
+            boolean clientAcc = client.next();
+            boolean photographerAcc = photographer.next();
+
+            if (clientAcc && photographerAcc) {
+                ArrayList<Client> clientList = (ArrayList) request.getAttribute("clientList");
+                ArrayList<Photographer> photographerList = (ArrayList) request.getAttribute("photographerList");
+
+            } else if (clientAcc) {
+
+                Client c = new Client();
+                c.getClientId();
+                c.getEmail();
+                c.getPassword();
+                c.getFname();
+                c.getLname();
+                c.getAddress_no();
+                c.getStreet();
+                c.getCity();
+                c.getGenderId();
+
+            } else if (photographerAcc) {
+                Photographer p = new Photographer();
+                p.getPhotographerId();
+                p.getEmail();
+                p.getPassword();
+                p.getFname();
+                p.getLname();
+                p.getAddress_no();
+                p.getStreet();
+                p.getCity();
+                p.getGenderId();
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } catch (Exception e) {
+        }
+    }
 }
