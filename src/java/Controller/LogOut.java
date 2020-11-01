388563c25a37c1b2237b0c5d74051718a98a4f2f
kesh4 <kesharabalasuriya11@gmail.com>
2020-11-01 07:52:35 +0530

    login filters

---
+++ src/java/Controller/LogOut.java
@@ -0,0 +1,41 @@
+package Controller;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author kesh
+ */
+public class LogOut extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        try {
+            String location = (String) request.getParameter("loc");
+            request.getSession().invalidate();
+            if (location.equals("ch")) {
+                response.sendRedirect("/GroupProject/View/Home.jsp");
+
+            } else {
+                response.sendRedirect("/GroupProject/View/login.jsp");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
