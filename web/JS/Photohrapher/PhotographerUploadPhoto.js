277ca96ebe92ceef2cb11c7bd9b13191e9c953c2
Aaketk17 <thavanthya@gmail.com>
2020-11-13 14:48:38 +0530

    Merge origin/master
    Conflicts:
    	web/CSS/Photographer/PhotographerProfile.css

--- web/JS/Photohrapher/PhotographerUploadPhoto.js
+++
@@ -1,6 +0,0 @@
-function changeimage(image) {
-    if (image == "ls1") {
-        document.getElementById("image").style.visibility = "hidden";
-        document.getElementById("ic1").style.visibility = "visible";
-    }
-}
