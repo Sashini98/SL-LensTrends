9d77d0958363788c24e43410afcaa4a335ade5d1
Sashini Shihara <sashinisr98@gmail.com>
2020-11-14 12:36:13 +0530

    bold function

--- web/JS/Forum/AskQues.js
+++ web/JS/Forum/AskQues.js
@@ -1,16 +1,3 @@
-$('#submit').click(function(){
-    var myTxt = $('.body').html();
-    alert(myTxt);
-    $.ajax({
-        type: 'post',
-        url:  '../../AskQues.java',
-        data: {data: myTxt}
-        
-    });
-
-});
-
-
 
 function DropDownChanged(oDDL) {
     var oTextbox = oDDL.form.elements["c_txt"];
@@ -29,27 +16,18 @@ function FormSubmit(oForm) {
         oHidden.value = (oDDL.value == "") ? oTextbox.value : oDDL.value;
 }
 
-function lbc(h, a, i) { 
-    var g = document.getElementById(h);
-    g.focus();
-    if (g.setSelectionRange) {
-        var c = g.scrollTop;
-        var e = g.selectionStart;
-        var f = g.selectionEnd;
-        g.value = g.value.substring(0, g.selectionStart) + a + g.value.substring(g.selectionStart, g.selectionEnd) + i + g.value.substring(g.selectionEnd, g.value.length);
-        g.selectionStart = e;
-        g.selectionEnd = f + a.length + i.length;
-        g.scrollTop = c;
-    } else {
-        if (document.selection && document.selection.createRange) {
-            g.focus();
-            var b = document.selection.createRange();
-            if (b.text != "") {
-                b.text = a + b.text + i;
-            } else {
-                b.text = a + "REPLACE" + i;
-            }
-            g.focus();
-        }
+
+function validate()
+{
+    var x = document.forms["askQ"]["title"].value;
+    if (x == "") {
+        alert("Title must be filled out");
+        return false;
     }
+
+//    var y = document.forms["askQ"]["body"].value;
+//    if (y == "") {
+//        alert("Question must be filled out");
+//        return false;
+//    }
 }
