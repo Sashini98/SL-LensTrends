4113ffb7de07df4394f79e04037f634b25967899
Sashini Shihara <sashinisr98@gmail.com>
2021-01-12 08:55:31 +0530

    pastEvents

---
+++ web/JS/Events/PastEvents.js
@@ -0,0 +1,35 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function view()
+{
+    var request = new XMLHttpRequest();
+
+    request.onreadystatechange = function () {
+
+        if (request.status === 200) {
+            if (request.readyState === 4) {
+
+
+
+                var responce = request.responseText;
+                document.getElementById("main-content").innerHTML = responce;
+
+
+            }
+        }
+
+    };
+
+
+
+
+    request.open("POST", "../../forumH", false);
+    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    request.send();
+
+}
