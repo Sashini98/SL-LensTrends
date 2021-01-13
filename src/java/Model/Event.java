6eb29ed3cde03e3f05c7bc61e54dce1aa46d745a
Sashini Shihara <sashinisr98@gmail.com>
2021-01-13 10:38:56 +0530

    db change-events table

--- src/java/Model/Event.java
+++ src/java/Model/Event.java
@@ -18,44 +18,134 @@ public class Event {
     String type;
     Date eventDate;
     /*time eventTime;*/
-    float estimatedBudget;
     String area;
-    String photographerId;
     String clientId;
     String hallNo;
     String venue;
     String street;
     String city;
     String province;
-    String comment; 
+   
     
     public Event()
     {}
     
-    public Event(String eventId,String eventName,String type,Date eventDate,/*time eventTime,*/float estimatedBudget,String area,String photographerId,String clientId,String hallNo,String venue,String street,String city,String province,String comment)
+    public Event(String eventId,String eventName,String type,Date eventDate,/*time eventTime,*/String area,String clientId,String hallNo,String venue,String street,String city,String province)
     {
         this.eventId=eventId;
         this.eventName=eventName;
         this.type=type;
         this.eventDate=eventDate;
         /* this.eventTime=eventTime;*/
-        this.estimatedBudget=estimatedBudget;
         this.area=area;
-        this.photographerId=photographerId;
         this.clientId=clientId;
         this.hallNo=hallNo;
         this.venue=venue;
         this.street=street;
         this.city=city;
         this.province=province;
-        this.comment=comment;
+        
     }
     
-    public String eventId()
-    {
+    public String geteventId() {
         return eventId;
     }
+
+    public void seteventId(String eventId) {
+        this.eventId = eventId;
+    }
     
+     public String geteventName()
+    {
+        return eventName;
+    }
     
+    public void seteventName(String eventName)
+    {
+        this.eventName=eventName;
+    }
     
+     public String gettype()
+    {
+        return type;
+    }
+    
+    public void settype(String type)
+    {
+        this.type=type;
+    }
+    
+     public Date geteventDate() {
+        return eventDate;
+    }
+
+    public void seteventDate(Date eventDate) {
+        this.eventDate = eventDate;
+    }
+    
+   public String getarea()
+    {
+        return area;
+    }
+    
+    public void setarea(String area)
+    {
+        this.area=area;
+    }
+    
+      public String getclientId() {
+        return clientId;
+    }
+
+    public void setclientId(String clientId) {
+        this.clientId = clientId;
+    }
+    
+    public String gethallNo()
+    {
+        return hallNo;
+    }
+    
+    public void sethallNo(String hallNo)
+    {
+        this.hallNo=hallNo;
+    }
+    
+    public String getvenue()
+    {
+        return venue;
+    }
+    
+    public void setvenue(String venue)
+    {
+        this.venue=venue;
+    }
+    
+    public String getstreet()
+    {
+        return street;
+    }
+    
+    public void setstreet(String street)
+    {
+        this.street=street;
+    }
+    public String getcity()
+    {
+        return city;
+    }
+    
+    public void setcity(String city)
+    {
+        this.city=city;
+    }
+    public String getprovince()
+    {
+        return province;
+    }
+    
+    public void setprovince(String province)
+    {
+        this.province=province;
+    }
 }
