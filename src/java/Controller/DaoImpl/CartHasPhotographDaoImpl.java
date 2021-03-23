3114f63ec14bb32d345b733930a1e8cb42297a8c
kesh4 <kesharabalasuriya11@gmail.com>
2021-03-23 06:35:52 +0530

    Merge origin/master
    Conflicts:
    	src/java/Controller/DaoImpl/PhotographDaoImpl.java
    	src/java/Model/Dao/PhotographDao.java
    	web/WEB-INF/web.xml

--- src/java/Controller/DaoImpl/CartHasPhotographDaoImpl.java
+++ src/java/Controller/DaoImpl/CartHasPhotographDaoImpl.java
@@ -16,17 +16,19 @@ import java.util.ArrayList;
  *
  * @author kesh
  */
-public class CartHasPhotographDaoImpl implements CartHasPhotographDao{
+public class CartHasPhotographDaoImpl implements CartHasPhotographDao {
 
     @Override
     public ArrayList<cart_has_photograph> getCartItems(String clientID) throws SQLException {
-        
-        ResultSet cartIdRes = DB.search("SELCET Cart_Id FROM cart WHERE Client_Id = '"+ clientID +"'");
-        
-        if (true) {
+
+        ResultSet cartIdRes = DB.search("SELCET Cart_Id FROM cart WHERE Client_Id = '" + clientID + "'");
+
+        if (cartIdRes.next()) {
+            ResultSet cartItems = DB.search("SELCET cart_has_photograph FROM cart WHERE Cart_Id = '" + cartIdRes.getInt("cartIdRes") + "'");
             
+
         }
         return new ArrayList<>();
     }
-    
+
 }
