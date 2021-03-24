a7ede14b3fdd05ea6bc286bdf1cda5134a84c689
kesh4 <kesharabalasuriya11@gmail.com>
2021-03-24 13:55:52 +0530

    cart

--- src/java/Controller/Clent/AddToCart.java
+++ src/java/Controller/Clent/AddToCart.java
@@ -5,8 +5,16 @@
  */
 package Controller.Clent;
 
+import Controller.DaoImpl.CartHasPhotographDaoImpl;
+import Model.Client;
+import Model.Dao.CartHasPhotographDao;
+import Model.Photograph;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -20,10 +28,27 @@ public class AddToCart extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String photoId = req.getParameter("no");
-        
-        
-        
-        
+        try {
+            int photoId = Integer.parseInt(req.getParameter("no"));
+            
+            Client client = (Client) req.getSession().getAttribute("loggedClient");
+            
+            CartHasPhotographDao chpd = new CartHasPhotographDaoImpl();
+            ArrayList<Photograph> cartItems = chpd.getCartItems(client.getClientId());
+            
+            for (Photograph cartItem : cartItems) {
+                if (cartItem.getId() == photoId) {
+                    req.setAttribute("error", "Image is already added!");
+                } else {
+                    CartHasPhotographDao dao = new CartHasPhotographDaoImpl();
+                    dao.addCartItem(client.getClientId(), photoId);
+                }
+
+            }
+            
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
     }
 }
