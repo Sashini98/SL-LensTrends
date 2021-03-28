6aa038ddcd3aed750a02f0211d2c88e39e23e888
Theivendram Athavan <61150758+Aaketk17@users.noreply.github.com>
2021-03-29 00:57:19 +0530

    Merge origin/master
    Conflicts:
    	web/WEB-INF/web.xml

--- src/java/Controller/Photographer/UploadforPortfolio.java
+++ src/java/Controller/Photographer/UploadforPortfolio.java
@@ -9,12 +9,22 @@ import Controller.DaoImpl.PhotographDaoImpl;
 import Model.Dao.PhotographDao;
 import Model.Photographer;
 import Model.portfolio_photograph;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.stream.ImageOutputStream;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -40,19 +50,42 @@ public class UploadforPortfolio extends HttpServlet {
 
             List<FileItem> fil = sfu.parseRequest(request);
             FileItem fi = fil.get(0);
-            
+            System.out.println(fi);
 
             String realpath = getServletContext().getRealPath("");
             String imagename = String.valueOf(System.currentTimeMillis());
-      
+//            String compressedimagename = String.valueOf(System.currentTimeMillis() + 5);
+//
             String filepath = "";
             filepath = realpath + "\\Resources\\Img\\Gallery Sample Images\\" + imagename + ".jpeg";
-//             System.out.println(filepath);
+////             System.out.println(filepath);
+//
+//            File input = new File(imagename + ".jpeg");
+//            BufferedImage image = ImageIO.read(input);
+//
+//            File compressedImageFile = new File(compressedimagename + ".jpeg");
+//            OutputStream os = new FileOutputStream(compressedImageFile);
+//
+//            Iterator<ImageWriter> writers = ImageIO.getImageWritersByFormatName("jpeg");
+//            ImageWriter writer = (ImageWriter) writers.next();
+//
+//            ImageOutputStream ios = ImageIO.createImageOutputStream(os);
+//            writer.setOutput(ios);
+//            
+//            ImageWriteParam param = writer.getDefaultWriteParam();
+//
+//            param.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+//            param.setCompressionQuality(0.05f);
+//            writer.write(null, new IIOImage(image, null, null), param);
+//
+//            os.close();
+//            ios.close();
+//            writer.dispose();
+            
             File file = new File(filepath);
             fi.write(file);
-            
+
 //            String title = request.getParameter("title");
-            
             int id = 0;
             String path = imagename + ".jpeg";
             String title = request.getParameter("title");
@@ -69,7 +102,6 @@ public class UploadforPortfolio extends HttpServlet {
 
             PhotographDao photoDao = new PhotographDaoImpl();
             photoDao.uploadphotoforportfolio(m);
-            
 
             response.getWriter().write("Successfully Uploaded");
 
